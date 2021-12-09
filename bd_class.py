4d9cdcbe45f559f0f65b9b2f84c86a15626465a1
gelllah <94802067+Llahainur@users.noreply.github.com>
2021-12-09 18:41:02 +0300

    tbot_class.py

--- bd_class.py
+++ bd_class.py
@@ -3,38 +3,32 @@ from classes import *
 
 
 def array_maker(Pers):
-    arr = []
-    arr.append(str(Pers.name))
-    arr.append(str(Pers.phone))
-    arr.append(float(Pers.geocode_arr['lat']))
-    arr.append(float(Pers.geocode_arr['lon']))
-    arr.append(str(Pers.geocode_arr['addr']))
-    arr.append(float(Pers.geocode_dest['lat']))
-    arr.append(float(Pers.geocode_dest['lon']))
-    arr.append(str(Pers.geocode_dest['addr']))
-    arr.append(int(Pers.is_driver))
+    arr = (str(Pers.name), str(Pers.phone), float(Pers.geocode_arr['lat']), float(Pers.geocode_arr['lon']),
+           str(Pers.geocode_arr['addr']), float(Pers.geocode_dest['lat']), float(Pers.geocode_dest['lon']),
+           str(Pers.geocode_dest['addr']), int(Pers.is_driver))
     return arr
+
 
 class BD(object):
     def __init__(self):
         self.name = "data.db"
 
     def adddata(P):
-        conn = sql.connect("data.db")
-        cur = conn.cursor()
+        connect = sql.connect("data.db")
+        curs = conn.cursor()
         id = float(P.geocode_arr["lat"]) * float(P.geocode_arr["lon"]) + float(P.geocode_dest["lat"]) + float(
-            P.geocode_dest["lat"])  # достаточно
-        # уникальный набор данных
-        print(array_maker(P))
-        #cur.executemany("")
-        #conn.commit()
+            P.geocode_dest["lat"])  # достаточно уникальный набор данных
+
+        data = array_maker(P)
+        cur.execute("INSERT INTO users VALUES(?,?,?,?,?,?,?,?,?);", array_maker(P))
+        connect.commit()
 
 
 if __name__ == "__main__":
     P = Person("Москва", "Казань", "Вася", "89898989898", True)
     conn = sql.connect('data.db')
     cur = conn.cursor()
-
+    print(array_maker(P))
     cur.execute("""CREATE TABLE IF NOT EXISTS users(
        userid INT PRIMARY KEY,
        name TEXT,
@@ -48,5 +42,5 @@ if __name__ == "__main__":
        isdriver INT
        );
     """)
-    conn.commit()
     BD.adddata(P)
+
