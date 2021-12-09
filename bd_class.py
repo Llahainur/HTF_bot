501bf17026228c4d683193c936fd60503bd64f78
gelllah <94802067+Llahainur@users.noreply.github.com>
2021-12-09 19:59:15 +0300

    tbot_class.py

--- bd_class.py
+++ bd_class.py
@@ -1,32 +1,38 @@
 import sqlite3 as sql
 from classes import Person
 
+bd_name = "data.db"
+
 
 def array_maker(Pers):
     arr = (str(Pers.name), str(Pers.phone), float(Pers.geocode_arr['lat']), float(Pers.geocode_arr['lon']),
            str(Pers.geocode_arr['addr']), float(Pers.geocode_dest['lat']), float(Pers.geocode_dest['lon']),
-           str(Pers.geocode_dest['addr']),int(Pers.is_driver))#isdriver
+           str(Pers.geocode_dest['addr']), int(Pers.is_driver))  # isdriver
     print(arr)
     return arr
 
 
 class BD(object):
     def __init__(self):
-        self.name = "data.db"
+        self.name = bd_name
 
-    def adddata(P):
-        connect = sql.connect("data.db")
+    def adddata(self, Pers):
+        connect = sql.connect(bd_name)
         curs = connect.cursor()
-        data = array_maker(P)
+        data = array_maker(Pers)
         curs.execute("INSERT INTO users VALUES(?,?,?,?,?,?,?,?,?);"
                      , data)
         connect.commit()
+        connect.close()
+
+    def readata(self):
+        connect = sql.connect(bd_name)
         print(connect.execute("SELECT * FROM users;"))
         connect.close()
 
 
-
 if __name__ == "__main__":
+    database = BD
     P = Person("Москва", "Казань", "Вася", "89898989898", True)
     conn = sql.connect('data.db')
     cur = conn.cursor()
@@ -41,5 +47,5 @@ if __name__ == "__main__":
        dest_addr,
        is_driver)""")
     conn.close()
-    BD.adddata(P)
-
+    BD.adddata(database, P)
+    BD.readata(database)
