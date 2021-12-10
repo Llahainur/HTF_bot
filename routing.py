c97d127951b55935c76f0719dc94c5da74a070f4
gelllah <94802067+Llahainur@users.noreply.github.com>
2021-12-10 23:20:18 +0300

    tbot_class.py

--- routing.py
+++ routing.py
@@ -1,40 +1,19 @@
 from classes import Person
 from bd_class import readdata
 from math import cos
+import time
 
 
-class Point:
+class Point:  # класс точки. Содержит 2 координаты - широту и долготу (x,y)
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
 
-class Path:
+class Path:  # класс пути. Содержит две точки - начала и конца пути.
     def __init__(self, pointS, pointE):
         self.pointS = pointS
         self.pointE = pointE
-
-
-def point_diff(point1=Point, point2=Point):
-    Klat = 111.12
-    Klon = 64  # по Москве
-    return round(((abs((point1.x - point2.x) * Klat) ** 2 + abs((point1.y - point2.y) * Klon) ** 2) ** 0.5), 2)
-
-
-def path_diff(path1=Path, path2=Path):
-    startdiff = point_diff(path1.pointS, path2.pointS)
-    enddiff = point_diff(path1.pointE, path2.pointE)
-    diff = startdiff * enddiff
-    return round(diff, 4), startdiff, enddiff
-
-
-def mass_path_diff(arr_of_Paths=[Path]):
-    res=[[]]
-    for i in range(len(arr_of_Paths)):
-        for j in range(i, len(arr_of_Paths)):
-            res[i][j] = path_diff(arr_of_Paths[i], arr_of_Paths[j])
-        print(res[i])
-    return res
 
 
 def recs_to_arr(records):
@@ -43,9 +22,44 @@ def recs_to_arr(records):
         p1 = Point(float(row[2]), float(row[3]))
         p2 = Point(float(row[5]), float(row[6]))
         path = Path(p1, p2)
-        # нач, коор конц, водитель?,номер записи
         P_array.append(path)
     return P_array
+
+
+def point_diff(point1=Point, point2=Point):  # ищет расстояние между двумя точками, переводя в километры. Точность 5%
+    Klat = 111.12
+    Klon = 64  # коэфф град->километр, по Москве
+    return round(((abs((point1.x - point2.x) * Klat) ** 2 + abs((point1.y - point2.y) * Klon) ** 2) ** 0.5), 2)
+
+
+def path_diff(path1=Path, path2=Path):
+    startdiff = point_diff(path1.pointS, path2.pointS)  # разность по начальным координатам
+    enddiff = point_diff(path1.pointE, path2.pointE)  # разность по конечным координатам
+    diff = startdiff * enddiff  # разность для обоих коорд. Определяющий критерий
+    return round(diff, 4), startdiff, enddiff
+
+
+def mass_path_diff(arr_of_Paths, search_id):
+    l = len(arr_of_Paths)
+    res = [0] * l
+    for i in range(l):
+        res[i] = [0] * l
+    found_id = 0
+
+    for i in range(len(arr_of_Paths)):
+        for j in range(i + 1, len(arr_of_Paths)):
+            res[i][j] = path_diff(arr_of_Paths[i], arr_of_Paths[j])
+            found_diff = res[i][j][0]
+
+            if i == search_id and res[i][j][0] < found_diff:
+                found_id = [i, j]
+                found_diff = res[i][j][0]
+            elif j == search_id and res[i][j][0] < found_diff:
+                found_id = [i, j]
+                found_diff = res[i][j][0]
+
+            print(path_diff(arr_of_Paths[i], arr_of_Paths[j]), i, j)
+    return res, found_id, found_diff
 
 
 if __name__ == "__main__":
@@ -55,4 +69,5 @@ if __name__ == "__main__":
     P1 = Path
     P2 = Path
     P1, P2 = P_arr[0], P_arr[1]
-    print(mass_path_diff(P_arr))
+    diff, id, found_d = mass_path_diff(P_arr, 2)
+    print(id, found_d)
